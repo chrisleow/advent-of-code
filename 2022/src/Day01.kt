38fecdeafea47a8a43263d62974eca21f9ba50ae
Chris Leow <chris@complyadvantage.com>
2022-12-01 10:13:18 +0000

    Functionalised a 'list-split' function.

--- 2022/src/Day01.kt
+++ 2022/src/Day01.kt
@@ -1,36 +1,24 @@
 fun main() {
 
-    fun getCalorieSets(input: List<String>): List<List<Int>> {
-        return input
-            .fold(listOf(emptyList<Int>())) { lines, line ->
-                if (line.isBlank()) {
-                    lines + listOf(emptyList())
-                } else {
-                    lines.dropLast(1) + listOf(lines.last() + line.toInt())
-                }
-            }
-            .filter { it.isNotEmpty() }
-    }
+    fun getCalorieSets(input: List<String>) = input
+        .split { it.isBlank() }
+        .map { lines -> lines.map { it.toInt() } }
+        .filter { it.isNotEmpty() }
 
-    fun part1(input: List<String>): Int {
-        return getCalorieSets(input)
-            .maxOf { it.sum() }
-    }
+    fun part1(input: List<String>) = getCalorieSets(input)
+        .maxOf { it.sum() }
 
-    fun part2(input: List<String>): Int {
-        return getCalorieSets(input)
-            .map { it.sum() }
-            .sortedDescending()
-            .take(3)
-            .sum()
-    }
+    fun part2(input: List<String>) = getCalorieSets(input)
+        .map { it.sum() }
+        .sortedDescending()
+        .take(3)
+        .sum()
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
     check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
--- 2022/src/Utils.kt
+++ 2022/src/Utils.kt
@@ -14,3 +14,32 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Split a sequence into sub-lists based on predicate
+ */
+fun <T> Sequence<T>.split(predicate: (T) -> Boolean): Sequence<List<T>> {
+    val source = this
+    return sequence {
+        var current = mutableListOf<T>()
+        source.forEach { item ->
+            if (predicate(item)) {
+                yield(current)
+                current = mutableListOf()
+            } else {
+                current.add(item)
+            }
+        }
+        if (current.isNotEmpty()) {
+            yield(current)
+        }
+    }
+}
+
+/**
+ * Split an iterable into sub-lists based on predicate
+ */
+fun <T> Iterable<T>.split(predicate: (T) -> Boolean): List<List<T>> = this
+    .asSequence()
+    .split(predicate)
+    .toList()

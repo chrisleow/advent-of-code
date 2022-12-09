546751575f6fb6ec3e8dba67f0c9522f63c362e0
Chris Leow <chris@complyadvantage.com>
2022-12-09 10:14:51 +0000

    Day09 solution

---
+++ 2022/src/Day09.kt
@@ -0,0 +1,70 @@
+import kotlin.math.abs
+
+fun main() {
+
+    data class Point(val x: Int, val y: Int)
+    data class Instruction(val direction: Char, val distance: Int)
+
+    val instructionRegex = "([LRUD])\\s+(\\d+)".toRegex()
+
+    fun List<String>.parse() = this
+        .mapNotNull { line -> instructionRegex.matchEntire(line)?.groupValues }
+        .map { gv -> Instruction(gv[1].first(), gv[2].toInt()) }
+
+    fun List<Instruction>.generateRopes(initialRope: List<Point>): Sequence<List<Point>> = sequence {
+        yield(initialRope)
+        this@generateRopes
+            .asSequence()
+            .flatMap { instruction -> List(instruction.distance) { instruction.direction } }
+            .fold(initialRope) { rope, direction ->
+                val newRope = rope.fold(emptyList<Point>()) { partialNewRope, tail ->
+                    partialNewRope + if (partialNewRope.isEmpty()) {
+                        when (direction) {
+                            'L' -> Point(tail.x - 1, tail.y)
+                            'R' -> Point(tail.x + 1, tail.y)
+                            'U' -> Point(tail.x, tail.y - 1)
+                            'D' -> Point(tail.x, tail.y + 1)
+                            else -> error("bad direction char ${direction}.")
+                        }
+                    } else {
+                        val head = partialNewRope.last()
+                        val deltaX = head.x - tail.x
+                        val deltaY = head.y - tail.y
+                        if (abs(deltaX) > 1 || abs(deltaY) > 1) {
+                            Point(
+                                x = tail.x + minOf(maxOf(deltaX, -1), 1),
+                                y = tail.y + minOf(maxOf(deltaY, -1), 1),
+                            )
+                        } else {
+                            tail
+                        }
+                    }
+                }
+
+                yield(newRope)
+                newRope
+            }
+    }
+
+    fun part1(input: List<String>) = input
+        .parse()
+        .generateRopes(listOf(Point(0, 0), Point(0, 0)))
+        .map { it.last() }
+        .distinct()
+        .count()
+
+    fun part2(input: List<String>) = input
+        .parse()
+        .generateRopes(List(10) { Point(0, 0) })
+        .map { it.last() }
+        .distinct()
+        .count()
+
+    val testInput = readInput("Day09_test")
+    check(part1(testInput) == 88)
+    check(part2(testInput) == 36)
+
+    val input = readInput("Day09")
+    println("Part 1 ${part1(input)}")
+    println("Part 2 ${part2(input)}")
+}
